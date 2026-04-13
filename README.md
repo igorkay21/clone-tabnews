69c6ac035d1ffcc6b497edbed67e0981783c4ad8
igorkay21 <igorkay2105@gmail.com>
2026-04-13 17:18:20 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para aprender programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Fé no Pai</h1>
+  return <h1>Fé no Pai</h1>;
+}
+
+function teste() {
+  console.log;
 }
 
 export default Home;
